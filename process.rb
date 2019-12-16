0dac169a5ba927668ee89dfa20aa1bf785cacb85
CaffeinatedDave <dave@caffeinateddave.com>
2019-12-16 08:45:41 +0000

    Clean up + more games played

--- process.rb
+++ process.rb
@@ -1,26 +1,26 @@
 require 'Date'
 
-prevHolder = "None"
-currentHolder = ARGV[0] || "Nottingham"
-htmlMode = ARGV.map{|f| f == "-h"}.any?
+prevHolder = 'None'
+currentHolder = ARGV[0] || 'Nottingham'
+htmlMode = ARGV.map{|f| f == '-h'}.any?
 
 data = {}
 
 def fullName(str)
   teams = {
-    "Belfast" => "Belfast Giants",
-    "Cardiff" => "Cardiff Devils",
-    "Coventry" => "Coventry Blaze",
-    "Dundee" => "Dundee Stars",
-    "Fife" => "Fife Flyers",
-    "Glasgow" => "Glasgow Clan",
-    "Guildford" => "Guildford Flames",
-    "Manchester" => "Manchester Storm",
-    "Nottingham" => "Nottingham Panthers",
-    "Sheffield" => "Sheffield Steelers",
-    "Hull" => "Hull Stingrays",
-    "Edinburgh" => "Edinburgh Capitals",
-    "MiltonKeynes" => "Milton Keynes Lightning"
+    'Belfast' => 'Belfast Giants',
+    'Cardiff' => 'Cardiff Devils',
+    'Coventry' => 'Coventry Blaze',
+    'Dundee' => 'Dundee Stars',
+    'Fife' => 'Fife Flyers',
+    'Glasgow' => 'Glasgow Clan',
+    'Guildford' => 'Guildford Flames',
+    'Manchester' => 'Manchester Storm',
+    'Nottingham' => 'Nottingham Panthers',
+    'Sheffield' => 'Sheffield Steelers',
+    'Hull' => 'Hull Stingrays',
+    'Edinburgh' => 'Edinburgh Capitals',
+    'MiltonKeynes' => 'Milton Keynes Lightning'
   }
 
   if teams[str] == nil
@@ -32,28 +32,28 @@ end
 
 def makeDefaultStats(str)
   return {
-    "teamName" => fullName(str),
-    "gamesSinceChallenged" => 0,
-    "totalFailedAttempts" => 0,
-    "failedAttempts" => [],
-    "streak" => 0,
-    "titles" => []
+    'teamName' => fullName(str),
+    'gamesSinceChallenged' => 0,
+    'totalFailedAttempts' => 0,
+    'failedAttempts' => [],
+    'streak' => 0,
+    'titles' => []
   }
 end
 
 streak = 0
-streakStart = "Season 2010-11 Champions"
+streakStart = 'Season 2010-11 Champions'
 data[currentHolder] = makeDefaultStats(currentHolder)
 
-File.open("gameData").each do |game|
-  dateEnc, score = game.split("  ")
+File.open('gameData').each do |game|
+  dateEnc, score = game.split('  ')
 
-  season, date, _ = dateEnc.split(" ")
+  season, date, _ = dateEnc.split(' ')
 
   seasonStart = season[0..3]
   seasonEnd = season[0..1] + season[4..5]
 
-  day, monthIdx, _ = date.split(".")
+  day, monthIdx, _ = date.split('.')
   month = Date::MONTHNAMES[monthIdx.to_i]
 
   dateStr = "#{day} #{month} "
@@ -63,7 +63,7 @@ File.open("gameData").each do |game|
     dateStr += seasonEnd
   end
 
-  win, wscore, _, lose, lscore, ot = score.split(" ")
+  win, wscore, _, lose, lscore, ot = score.split(' ')
 
   # Make sure our teams exist for stats!
   if data[win] == nil
@@ -77,8 +77,8 @@ File.open("gameData").each do |game|
   lscore = lscore[1..-2]
 
   if win == currentHolder or lose == currentHolder
-     data[win]["gamesSinceChallenged"] = 0
-     data[lose]["gamesSinceChallenged"] = 0
+     data[win]['gamesSinceChallenged'] = 0
+     data[lose]['gamesSinceChallenged'] = 0
 
      challenger = if (win == currentHolder)
        lose
@@ -91,69 +91,69 @@ File.open("gameData").each do |game|
      if ot == nil and lose == currentHolder
 
        oldReign = {
-         "start" => streakStart,
-         "end" => dateStr,
-         "streak" => streak,
-         "to" => fullName(win),
-         "from" => fullName(prevHolder)
+         'start' => streakStart,
+         'end' => dateStr,
+         'streak' => streak,
+         'to' => fullName(win),
+         'from' => fullName(prevHolder)
        }
-       data[lose]["titles"] << oldReign
+       data[lose]['titles'] << oldReign
 
-       puts "#{dateStr}: #{win} is the new champion, defeating #{lose} #{wscore} to #{lscore} after #{streak} games"
+#       puts "#{dateStr}: #{win} is the new champion, defeating #{lose} #{wscore} to #{lscore} after #{streak} games"
        streakStart = dateStr
        prevHolder = currentHolder
        currentHolder = win
-       data[win]["failedAttempts"] = []
+       data[win]['failedAttempts'] = []
        streak = 1
      else
        streak += 1
        if ot == nil
-         puts "#{dateStr}: #{currentHolder} is still the champion, defeating #{lose} #{wscore} to #{lscore}. Streak of #{streak}"
-         data[lose]["totalFailedAttempts"] += 1
-         data[lose]["failedAttempts"] << game
+#         puts "#{dateStr}: #{currentHolder} is still the champion, defeating #{lose} #{wscore} to #{lscore}. Streak of #{streak}"
+         data[lose]['totalFailedAttempts'] += 1
+         data[lose]['failedAttempts'] << game
        else
-         puts "#{dateStr}: #{currentHolder} is still the champion, despite losing #{wscore} - #{lscore} to #{win} AOT. Streak of #{streak}"
-         data[win]["totalFailedAttempts"] += 1
-         data[win]["failedAttempts"] << game
+#         puts "#{dateStr}: #{currentHolder} is still the champion, despite losing #{wscore} - #{lscore} to #{win} AOT. Streak of #{streak}"
+         data[win]['totalFailedAttempts'] += 1
+         data[win]['failedAttempts'] << game
        end
      end
   else
-    data[win]["gamesSinceChallenged"] += 1
-    data[win]["streak"] += 1
-    data[lose]["gamesSinceChallenged"] += 1
-    data[lose]["streak"] = 0
+    data[win]['gamesSinceChallenged'] += 1
+    data[win]['streak'] += 1
+    data[lose]['gamesSinceChallenged'] += 1
+    data[lose]['streak'] = 0
   end
 end
 
 # Don't forget current champion!
 currentReign = {
-  "start" => streakStart,
-  "end" => "current",
-  "streak" => streak,
-  "to" => nil,
-  "from" => fullName(prevHolder)
+  'start' => streakStart,
+  'end' => 'current',
+  'streak' => streak,
+  'to' => nil,
+  'from' => fullName(prevHolder)
 }
-data[currentHolder]["titles"] << currentReign
+data[currentHolder]['titles'] << currentReign
 
 data.sort.each do |k, v|
   if htmlMode 
     puts "<h4>#{v['teamName']}</h4>"
     puts "<p>#{v['titles'].length} Time Champions</p>"
-    puts "<table>"
-    puts "  <tr><th>Start Date</th><th>Previous Holders</th><th>End Date</th><th>Duration</th></tr>"
+    puts '<table class="table">'
+    puts '  <tr><th>Start Date</th><th>Previous Holders</th><th>End Date</th><th>Duration</th></tr>'
   else
     puts k
     puts "#{v['titles'].length} Time Champions"
   end
   teamGames = 0
-  v["titles"].each do |t|
+  v['titles'].each do |t|
     tFormated = "#{t['start']} (from #{t['from']}) - #{t['end']}: #{t['streak']} games"
     hFormated = "  <tr><td>#{t['start']}</td><td>#{t['from']}</td><td>#{t['end']}</td><td>#{t['streak']} games</td></tr>"
     if t['to'] != nil
       tFormated += " (to #{t['to']})"
     end
     if htmlMode 
-      puts "#{hFormated}"
+      puts hFormated
     else
       puts " - #{tFormated}"
     end
@@ -164,7 +164,7 @@ data.sort.each do |k, v|
   titlePercent = (teamGames * 100.0) / (titleGames + 0.0)
 
   if htmlMode 
-    puts "</table>"
+    puts '</table>'
   else
 
     puts "#{teamGames} Total Games As Champion"
@@ -177,6 +177,6 @@ data.sort.each do |k, v|
 
     puts "#{v['titles'].length} : #{v['titles'].length + v['totalFailedAttempts']} Win rating when challenging (#{challengePercent}%)"
     puts "#{teamGames} : #{titleGames} Win rating in title games (#{titlePercent}%)"
-    puts "-----"
+    puts '-----'
   end
 end
